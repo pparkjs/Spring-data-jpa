fd5ecbfc52ea671356de6f4acb31be81209f8f73
pparkjs <qkrwjdtn963@gmail.com>
2024-01-16 17:10:16 +0900

    spring data jpa 이용한 Auditing 설정

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -10,4 +17,16 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorAware(){
+		/*
+			return new AuditorAware<String>(){
+				@Override
+				public Optinal<String> getCurrentAuditor(){
+					return Optional.of(UUID.randomUUID().toString());
+				}
+		 */
+		// 실제로는 저 UUID부분에 SpringSecurity ContextHolder에서 Id 꺼내서 삽입!
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
