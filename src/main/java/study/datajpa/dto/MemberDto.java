edbffa44d6c105a1d9a9b9a0773f9f76d04ef7b6
pparkjs <qkrwjdtn963@gmail.com>
2024-01-16 17:11:05 +0900

    편리한 생성자 생성

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,6 +1,10 @@
 package study.datajpa.dto;
 
+import jakarta.persistence.EntityManager;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @Data
 public class MemberDto {
@@ -13,5 +17,10 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member){
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
